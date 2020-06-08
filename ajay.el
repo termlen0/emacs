83e25a4d236725a49d011e8ef4b3006ec24860d8
Ajay Chenampara <termlen0@achenamp-mac.attlocal.net>
2020-06-08 04:35:05 -0400

    emacs configs

---
+++ ajay.el
@@ -0,0 +1,51 @@
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c m") 'org-pomodoro) ; start pomo timer
+
+
+
+;; ;; hooks for pomodoro org mode
+(defun popup_alert (title message)
+  (require 'alert)
+  (setq alert-default-style 'libnotify)
+  (setq alert-fade-time '10)
+  (setq alert-persist-idle-time '120)
+  (alert (format "\"%s\" : \"%s\"" message title)))
+
+(add-hook 'org-pomodoro-finished-hook
+          (lambda ()
+            (popup_alert "pomodoro completed!" "time for a break.")))
+
+(add-hook 'org-pomodoro-break-finished-hook
+          (lambda ()
+            (popup_alert "pomodoro short break finished" "ready for another?")))
+
+(add-hook 'org-pomodoro-long-break-finished-hook
+          (lambda ()
+            (popup_alert "pomodoro long break finished" "ready for another?")))
+
+(add-hook 'org-pomodoro-killed-hook
+          (lambda ()
+            (popup_alert "pomodoro killed" "one does not simply kill a pomodoro!")))
+
+
+
+;; enable folding for xml
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+(setq projectile-project-search-path '("~/Projects/"))
+
+(add-hook 'yaml-mode-hook 'indent-guide-mode)
+(add-hook 'python-mode-hook 'indent-guide-mode)
---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("goals.org")))
+ '(package-selected-packages
+   (quote
+    (indent-guide org-pomodoro yaml-mode markdown-mode geiser lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
